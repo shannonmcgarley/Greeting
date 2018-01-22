94e9faf812b5453bb3d72462fd8d307b8d65cc00
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-01-22 12:11:08 +0000

    end of task 1

--- src/main/scala/App/GreetingApplication.scala
+++ src/main/scala/App/GreetingApplication.scala
@@ -3,7 +3,13 @@ package App
 import scala.io.StdIn
 
 object GreetingApplication extends App {
-  def greet(name: String) : Unit = println("hello " + name)
-val name = StdIn.readLine("What is you name? ")
-greet(name)
+  def greet(name: String) : Unit = {
+  if (name == "Shannon") {
+    println("You don't get a hello. ")
+  } else  {
+    println("Hello " + name)
+  }
+  }
+  val name = StdIn.readLine("What is your name?")
+  greet(name)
 }
