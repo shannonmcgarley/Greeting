42668442fe042c7c52aada7cf3930c09e3606e4d
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-01-23 12:02:40 +0000

    added promt for Whats your name

--- src/main/scala/App/GreetingApplication.scala
+++ src/main/scala/App/GreetingApplication.scala
@@ -2,14 +2,22 @@ package App
 
 import scala.io.StdIn
 
+
+class Person(name : String){
+
+  def speak() : String = {
+    if (name == "Shannon") {
+      "You dont get a hello."
+    } else {
+      "Hello " + name
+    }
+  }
+}
+object Promt {
+  def ask(message : String) : String = StdIn.readLine(message)
+}
 object GreetingApplication extends App {
-  def greet(name: String) : Unit = {
-  if (name == "Shannon") {
-    println("You don't get a hello. ")
-  } else  {
-    println("Hello " + name)
-  }
-  }
-  val name = StdIn.readLine("What is your name?")
-  greet(name)
+  val name = Promt.ask("What is your name?")
+  val p = new Person(name)
+  println(p.speak())
 }
