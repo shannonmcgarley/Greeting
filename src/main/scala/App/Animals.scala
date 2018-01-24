af3efc5febd8100192e0da420bcbb855321443e5
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-01-24 13:55:01 +0000

    adding new extentions with complex speech

--- src/main/scala/App/Animals.scala
+++ src/main/scala/App/Animals.scala
@@ -2,11 +2,17 @@ package App
 
 object Animals  extends App {
 
+  val Persian: Cat = new Persian()
+  val Wolf : Dog = new Wolf()
+  val Boarder : Dog = new Boarder()
+  val Meowth : Cat = new Meowth(100, true)
   val Jess : Animal = new Cat()
   val Lassie : Animal = new Dog()
 
-  println(Jess.speak)
-  println(Lassie.speak)
+  println(Persian.speak)
+  println(Wolf.speak)
+  println(Meowth.speak)
+  println(Boarder.speak)
 
 }
 
@@ -20,5 +26,28 @@ class Cat() extends Animal {
 }
 
 class Dog() extends Animal{
+  override def speak: String = "Meow"
+}
+
+class Persian() extends Cat {
+  override def speak: String = "Meow!"
+}
+class Wolf() extends Dog {
+    override def speak: String = "Howl!"
+}
+
+class Meowth(coins: Int, teamrocket: Boolean) extends Cat{
+  override def speak: String = {
+    val message = s"That's Right!, I have $coins coins "
+    if (teamrocket) {
+      s"$message I am in team rocket"
+    }
+    else{
+      s"$message I am not in team rocket"
+    }
+  }
+}
+
+class Boarder() extends Dog{
   override def speak: String = "Woof!"
 }
